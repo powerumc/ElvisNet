70f423bc1c12086d9a1699c96167deb8885abc63
엄준일 [umc] <umc@nexon.corp>
2016-01-28 17:47:47 +0900

    commit first

---
+++ ElvisNet/Elvis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ElvisNet
+{
+	public class Elvis
+	{
+		public Elvis ()
+		{
+		}
+
+		public static object Value<T>(Expression<Func<T>> expression)
+		{
+			var visitor = new ElvisExpressionVisitor ();
+			return visitor.Visit (expression);
+		}
+	}
+}
+
---
+++ ElvisNet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElvisNet
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var name = "powerumc";
+			var value = Elvis.Value (() => name);
+
+			Console.WriteLine (value);
+		}
+	}
+}
